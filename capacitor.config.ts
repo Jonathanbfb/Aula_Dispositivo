b7a7b1d0fbd7ac80b07110c1ca21800a631b49e1
JonathanBarros-ti <jonathanbrunoferreirabarros2@gmail.com>
2025-05-15 19:21:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aula15-05',
+  webDir: 'dist'
+};
+
+export default config;
