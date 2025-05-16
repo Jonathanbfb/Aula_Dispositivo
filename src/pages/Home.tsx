94d21a399db1a721408e4fc649714755e3944807
JonathanBarros-ti <jonathanbrunoferreirabarros2@gmail.com>
2025-05-15 21:31:36 -0400

    firs commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonInput,IonButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
-
+import { useState } from 'react';
 const Home: React.FC = () => {
+  const [lado, setLado] = useState (0);
+  const [area, setArea] = useState <number | null> (null);
+  const CalcQuad = () =>{
+    setArea(lado * lado)
+  }
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Cálculo da área do Quadrado</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +20,12 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        
+        <IonInput onIonChange={(e) => setLado(parseFloat(e.detail.value!)) } label="Digite o valor de um Lado do quadrado" placeholder="Digite o valor"></IonInput>
+        <IonButton onClick={CalcQuad} color="success">Calcular</IonButton>
+        <h2>  O valor da área é: {area}</h2>
+
+
       </IonContent>
     </IonPage>
   );
